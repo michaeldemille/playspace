37ec835c700f5fe69ea7b17a762ce063d838a3dd
Michael DeMille <michaeldemille@gmail.com>
2022-12-07 21:24:31 -0700

    added comment to explain adding unsplash creds

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 import { creds } from 'creds/creds';
 
+// to try this out you'll need to setup and use your unsplash api creds
+// @ https://unsplash.com/oauth/applications and create a free test app.
 export const environment = {
   production: true,
   unsplash: creds.unsplash,
